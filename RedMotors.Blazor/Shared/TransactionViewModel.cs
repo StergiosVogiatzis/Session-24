4f61a096e7005038895d70878bd2a30ae28c11c0
Stergios Vogiatzis <tehvog@gmail.com>
2022-04-07 00:32:19 +0300

    Transactions Selects first try

---
+++ RedMotors.Blazor/Shared/TransactionViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedMotors.Blazor.Shared
+{
+    public class TransactionViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid ManagerId { get; set; }
+        public Guid CarId { get; set; }
+        public Guid CustomerId { get; set; }
+        public decimal TotalPrice { get; set;}
+
+    }
+    public class TransactionEditViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid ManagerId { get; set; }
+        public Guid CarId { get; set; }
+        public Guid CustomerId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<TransactionLineViewModel> TransactionLines { get; set; } = new();
+    }
+    public class TransactionLineEditViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid TransactionId { get; set; }
+        public Guid ServiceTaskId { get; set; }
+        public Guid EngineerId { get; set; }
+        public decimal PricePerHour { get; set; } = 44.5m;
+        public decimal Hours { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
