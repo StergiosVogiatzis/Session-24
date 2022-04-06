3bf558485db84ff66773e44c22ea4224fa37b309
Stergios Vogiatzis <tehvog@gmail.com>
2022-04-06 17:56:35 +0300

    First Try transactions

---
+++ RedMotors.Blazor/Shared/TransactionLineViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedMotors.Blazor.Shared
+{
+    public class TransactionLineViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid ServiceTaskId { get; set; }
+        public Guid EngineerId { get; set; }
+        public Guid TransactionId { get; set; }
+        public decimal Hours { get; set;}
+        public decimal PricePerHour { get; set; } = 44.5m;
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ src/RedMotors.Database/Repository/TransactionLineRepo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RedMotors.Entities;
+
+namespace RedMotors.Database.Repository
+{
+    public class TransactionLinesRepo : IEntityRepo<TransactionLine>
+    {
+        private readonly GarageContext _context;
+        public TransactionLinesRepo(GarageContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(TransactionLine entity)
+        {
+            await _context.TransactionLines.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var foundTransactionLine = await _context.TransactionLines.SingleOrDefaultAsync(transactionLine => transactionLine.Id == id);
+            if (foundTransactionLine is null)
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            _context.TransactionLines.Remove(foundTransactionLine);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<TransactionLine>> GetAllAsync()
+        {
+            return await _context.TransactionLines.ToListAsync();
+        }
+
+        public async Task<TransactionLine?> GetByIdAsync(Guid id)
+        {
+            return await _context.TransactionLines.Where(transaction => transaction.Id == id).SingleOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(Guid id, TransactionLine entity)
+        {
+            var currentTransactionLine = await _context.TransactionLines.SingleOrDefaultAsync(transactionLine => transactionLine.Id == id);
+            if (currentTransactionLine is null)
+                throw new KeyNotFoundException($"Given id '{id}' was not found in database");
+            currentTransactionLine.TransactionId = entity.TransactionId;
+            currentTransactionLine.ServiceTaskId = entity.ServiceTaskId;
+            currentTransactionLine.EngineerId = entity.EngineerId;
+            //currentTransactionLine.ServiceTask = entity.ServiceTask;
+            currentTransactionLine.Engineer = entity.Engineer;
+            currentTransactionLine.Hours = entity.Hours;
+            await _context.SaveChangesAsync();
+        }
+
+        Task<IEnumerable<TransactionLine>> IEntityRepo<TransactionLine>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task AddAsync(TransactionLine entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
