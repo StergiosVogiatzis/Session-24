3d88a2ef8202100019f3b55fad6774584a4fc7ad
Dimitris Mantikidis <dimitrismad@hotmail.com>
2022-04-07 18:08:51 +0300

    Changes to EntityFramework

--- src/RedMotors.Entities/Manager.cs
+++ src/RedMotors.Entities/Manager.cs
@@ -3,6 +3,7 @@
 public class Manager : Person
 {
     public decimal SalaryPerMonth { get; set; }
+    public ICollection<Engineer> Engineers { get; set; }
     //public Person Person { get; set; }
 
     //public Manager(decimal salaryPerMonth)
